5e88a5a74239e732875101dd76d0898f65b36dd0
Rudyard Richter <rr@anthropic.com>
2024-05-09 15:26:28 -0700

    Fix TestCopySingleS3ObjectToLocalWithDestinationWildcard

--- e2e/cp_test.go
+++ e2e/cp_test.go
@@ -211,38 +211,44 @@ func runTestCopySingleObjectToLocalJSON(t *testing.T, tc *testCase) {
 
 // cp s3://bucket/object *
 func TestCopySingleS3ObjectToLocalWithDestinationWildcard(t *testing.T) {
-	t.Parallel()
-
 	for _, tc := range testCases {
+		tc := tc
 		t.Run(tc.storage, func(t *testing.T) {
-			t.Parallel()
-			s3client, s5cmd := setup(t)
-			bucket := s3BucketFromTestName(t)
-			createBucket(t, s3client, bucket)
-
-			const (
-				filename = "testfile1.txt"
-				content  = "this is a file content"
-			)
-
-			putFile(t, s3client, bucket, filename, content)
-
-			cmd := s5cmd("cp", tc.storage+"://"+bucket+"/"+filename, "*")
-			result := icmd.RunCmd(cmd)
-			result.Assert(t, icmd.Expected{ExitCode: 1})
-
-			// ignore stdout. we expect error logs from stderr.
-			assertLines(t, result.Stderr(), map[int]compareFunc{
-				0: equals(`ERROR "cp %v://%v/%v *": target "*" can not contain glob characters`, tc.storage, bucket, filename),
-			})
-
-			// assert local filesystem
-			expected := fs.Expected(t)
-			assert.Assert(t, fs.Equal(cmd.Dir, expected))
-			// assert s3 object
-			assert.Assert(t, ensureS3Object(s3client, bucket, filename, content))
+			runTestCopySingleS3ObjectToLocalWithDestinationWildcard(t, &tc)
 		})
 	}
+}
+
+func runTestCopySingleS3ObjectToLocalWithDestinationWildcard(t *testing.T, tc *testCase) {
+	t.Parallel()
+	t.Run(tc.storage, func(t *testing.T) {
+		t.Parallel()
+		s3client, s5cmd := setup(t)
+		bucket := s3BucketFromTestName(t)
+		createBucket(t, s3client, bucket)
+
+		const (
+			filename = "testfile1.txt"
+			content  = "this is a file content"
+		)
+
+		putFile(t, s3client, bucket, filename, content)
+
+		cmd := s5cmd("cp", tc.storage+"://"+bucket+"/"+filename, "*")
+		result := icmd.RunCmd(cmd)
+		result.Assert(t, icmd.Expected{ExitCode: 1})
+
+		// ignore stdout. we expect error logs from stderr.
+		assertLines(t, result.Stderr(), map[int]compareFunc{
+			0: equals(`ERROR "cp %v://%v/%v *": target "*" can not contain glob characters`, tc.storage, bucket, filename),
+		})
+
+		// assert local filesystem
+		expected := fs.Expected(t)
+		assert.Assert(t, fs.Equal(cmd.Dir, expected))
+		// assert s3 object
+		assert.Assert(t, ensureS3Object(s3client, bucket, filename, content))
+	})
 }
 
 // cp s3://bucket/prefix/ dir/
